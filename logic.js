04de38e3ae1617138c7e8a5910641624cba47121
Pisakrus <90222492+Pisakrus@users.noreply.github.com>
2026-05-03 16:41:39 +0200

    Created rowsToClear() function

--- logic.js
+++ logic.js
@@ -11,6 +11,18 @@ function createBoard(config) {
         board.push(row);
     };
     
-    return board
-}
+    return board;
+};
+
+// Checks full rows and returns their index in a list.
+function rowsToClear(board) {
+    let fullRows = [];
+
+    for(let y = 0; y < board.length; y++) {
+        if (board[y].every(x => x !== 0)) {
+            fullRows.push(y);
+        };
+    };
+    return fullRows; 
+};      
 
