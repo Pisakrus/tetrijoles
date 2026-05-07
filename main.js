14a8b669ed1f83a678e58624555147147cc60ddc
Pisakrus <90222492+Pisakrus@users.noreply.github.com>
2026-05-08 00:13:14 +0200

    Create main loop

--- main.js
+++ main.js
@@ -7,3 +7,32 @@ game.state.board[3][4] = 3
 
 createCanvas(game)
 DrawCanvasBoard(game)
+
+
+
+//---Game loop---
+
+let LastTime = 0;
+let dropTimer = 0;
+// Render at 30 FPS
+const FPS = 30;
+const STEP = 1000 / FPS;
+
+
+
+function gameLoop(timestamp) {
+    let deltaTime = timestamp - LastTime;
+    dropTimer += deltaTime;
+
+    if (dropTimer > STEP) {
+        // Update logic and input (placeholder)
+        dropTimer -= STEP;
+    };
+    DrawCanvasBoard(game);
+
+    LastTime = timestamp;
+
+    requestAnimationFrame(gameLoop);
+};
+
+requestAnimationFrame(gameLoop);
